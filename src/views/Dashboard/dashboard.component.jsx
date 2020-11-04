a737fe7cc2b1ebb78f3b3f9a16da0f0b994010c2
Varun Vishal <varun.vishal@prithvi.ai>
2020-11-04 20:07:45 +0530

    icons added

--- src/views/Dashboard/dashboard.component.jsx
+++ src/views/Dashboard/dashboard.component.jsx
@@ -25,8 +25,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://prithvi.ai/">
+        Prithvi AI
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
