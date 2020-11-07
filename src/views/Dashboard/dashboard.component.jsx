972ac4e9ec8e968f9f6b793084d72d0683d44b50
Varun Vishal <varunvishal@Varuns-MacBook-Air.local>
2020-11-07 19:43:17 +0530

    New icons added

--- src/views/Dashboard/dashboard.component.jsx
+++ src/views/Dashboard/dashboard.component.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MainListItems from './MainListItems';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import './dashboard.styles.scss'
-
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import MainListItems from "./main-list-items";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import "./dashboard.styles.scss";
+import { Bell, User, Menu as MenuIcon } from "react-feather";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://www.prithvi.ai/">
         Prithvi.AI
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -41,32 +38,30 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   toolbar: {
     paddingRight: 24, // keep right padding when drawer closed
   },
   toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
     ...theme.mixins.toolbar,
   },
- 
+
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
-     
-
     }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -75,38 +70,38 @@ const useStyles = makeStyles((theme) => ({
     marginRight: 36,
   },
   menuButtonHidden: {
-    display: 'none',
+    display: "none",
   },
   title: {
     flexGrow: 1,
   },
   drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
+    position: "relative",
+    whiteSpace: "nowrap",
     width: drawerWidth,
-    backgroundColor:'#20253b',
-    color:'#fff',
-    transition: theme.transitions.create('width', {
+    backgroundColor: "#20253b",
+    color: "#fff",
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: theme.spacing(9),
     },
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
+    height: "100vh",
+    overflow: "auto",
   },
   container: {
     paddingTop: theme.spacing(4),
@@ -114,10 +109,10 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-    borderRadius:'10px',
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+    borderRadius: "10px",
   },
   fixedHeight: {
     height: 240,
@@ -125,23 +120,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Dashboard() {
-    const [auth, setAuth] = React.useState(true);
-    
+  const [auth, setAuth] = React.useState(true);
+
   const classes = useStyles();
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open1 = Boolean(anchorEl);
 
-
-  const handleMenu = event => {
+  const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
   const handleClose = () => {
     setAnchorEl(null);
   };
-
-
 
   const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
@@ -155,24 +147,36 @@ export default function Dashboard() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)} style={{backgroundColor:'#fff'}}>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+        style={{ backgroundColor: "#fff" }}
+      >
         <Toolbar className={classes.toolbar}>
-         
           <IconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
           >
-            <MenuIcon style={{color:'black'}}/>
+            <MenuIcon style={{ color: "black" }} />
           </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
             {/* Dashboard */}
           </Typography>
-          <IconButton color="inherit">
+          <IconButton color="inherit" style={{ marginRight: "10px" }}>
             <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon style={{color:'black'}}/>
+              <Bell style={{ color: "black" }} size={22} />
             </Badge>
           </IconButton>
           {auth && (
@@ -184,19 +188,19 @@ export default function Dashboard() {
                 onClick={handleMenu}
                 color="white"
               >
-                <AccountCircle fontSize="medium" style={{color:'black'}} />
+                <User fontSize="medium" size={22} style={{ color: "black" }} />
               </IconButton>
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={open1}
                 onClose={handleClose}
@@ -214,18 +218,19 @@ export default function Dashboard() {
           paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
         }}
         open={open}
-        
       >
         <div className={classes.toolbarIcon}>
-           <div><h3>Prithvi Platform</h3></div>
+          <div>
+            <h3>Prithvi Platform</h3>
+          </div>
           <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon style={{color:'#fff',margin:'0'}}/>
+            <ChevronLeftIcon style={{ color: "#fff", margin: "0" }} />
           </IconButton>
         </div>
-        <Divider style={{backgroundColor:'#353d62'}}/>
+        <Divider style={{ backgroundColor: "#353d62" }} />
         {/* <List>{mainListItems}</List> */}
-        <MainListItems/>
-        <Divider style={{backgroundColor:'#353d62'}}/>
+        <MainListItems />
+        <Divider style={{ backgroundColor: "#353d62" }} />
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
@@ -233,11 +238,8 @@ export default function Dashboard() {
           <Grid container spacing={3}>
             {/* Chart */}
             <Grid item xs={12} md={12} lg={12}>
-              <Paper className={fixedHeightPaper}>
-                DUMMY TEXT
-              </Paper>
+              <Paper className={fixedHeightPaper}>DUMMY TEXT</Paper>
             </Grid>
-            
           </Grid>
           <Box pt={4}>
             <Copyright />
--- src/views/Dashboard/main-list-items.jsx
+++ src/views/Dashboard/main-list-items.jsx
@@ -2,22 +2,12 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import {Grid, Gitlab, Code, Settings, Database, Home, Star, FilePlus, Feather, List as TaigaIcon} from 'react-feather';
 import Divider from '@material-ui/core/Divider';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import CodeIcon from '@material-ui/icons/Code';
-import StorageIcon from '@material-ui/icons/Storage';
-import SettingsIcon from '@material-ui/icons/Settings';
-import AccountTreeIcon from '@material-ui/icons/AccountTree';
-
 import Collapse from '@material-ui/core/Collapse';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import List from '@material-ui/core/List';
-import HomeIcon from '@material-ui/icons/Home';
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import GradeIcon from '@material-ui/icons/Grade';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
 
 export default function MainListItems() {
   const [open, setOpen] = React.useState(false);
@@ -32,7 +22,7 @@ export default function MainListItems() {
         {/* from */}
         <ListItem button onClick={handleClick}>
           <ListItemIcon>
-            <DashboardIcon style={{ color: 'white' }} /> 
+            <Grid style={{ color: 'white' }} /> 
           </ListItemIcon>
           <ListItemText primary="Dashboard" />
           {open ? <ExpandLess /> : <ExpandMore />}
@@ -44,7 +34,7 @@ export default function MainListItems() {
             {/* <Link to="/home" style={{ textDecoration: 'none', color: 'white' }}> */}
               <ListItem button style={{backgroundColor:'#4d516831'}}>
                 <ListItemIcon>
-                    <HomeIcon style={{ color: 'white' }} size="small"/>
+                    <Home style={{ color: 'white' }}/>
                 </ListItemIcon>
                 <ListItemText primary="Home" style={{ color: 'white' }} />
               </ListItem>
@@ -54,7 +44,7 @@ export default function MainListItems() {
               {' '}
               <ListItem button style={{backgroundColor:'#4d516831'}}>
                 <ListItemIcon>
-                    <LibraryBooksIcon style={{ color: 'white' }}/>
+                    <Feather style={{ color: 'white' }}/>
                 </ListItemIcon>
                 <ListItemText primary="Projects" style={{ color: 'white' }}/>
               </ListItem>
@@ -62,7 +52,7 @@ export default function MainListItems() {
 
               <ListItem button style={{backgroundColor:'#4d516831'}}>
                 <ListItemIcon>
-                    <GradeIcon style={{ color: 'white' }}/>
+                    <Star style={{ color: 'white' }}/>
                 </ListItemIcon>
                 <ListItemText primary="Opportunities" style={{ color: 'white' }}/>
               </ListItem>
@@ -70,7 +60,7 @@ export default function MainListItems() {
 
               <ListItem button style={{backgroundColor:'#4d516831'}}>
                 <ListItemIcon >
-                    <NoteAddIcon style={{ color: 'white' }}/>
+                    <FilePlus style={{ color: 'white' }}/>
                 </ListItemIcon>
                 <ListItemText primary="Proposals" style={{ color: 'white' }}/>
               </ListItem>
@@ -85,7 +75,7 @@ export default function MainListItems() {
         >
           <ListItem button>
             <ListItemIcon>
-              <AccountTreeIcon style={{ color: 'white' }} />
+              <TaigaIcon style={{ color: 'white' }} />
             </ListItemIcon>
             <ListItemText primary="Taiga" />
           </ListItem>
@@ -99,7 +89,7 @@ export default function MainListItems() {
         >
           <ListItem button>
             <ListItemIcon>
-              <GitHubIcon style={{ color: 'white' }} />
+              <Gitlab style={{ color: 'white' }} />
             </ListItemIcon>
             <ListItemText primary="Git" />
           </ListItem>
@@ -107,14 +97,14 @@ export default function MainListItems() {
         <Divider style={{backgroundColor:'#353d62'}}/>
         <ListItem button>
           <ListItemIcon>
-            <CodeIcon style={{ color: 'white' }} />
+            <Code style={{ color: 'white' }} />
           </ListItemIcon>
           <ListItemText primary="Coding Env" />
         </ListItem>
         <Divider style={{backgroundColor:'#353d62'}}/>
         <ListItem button>
           <ListItemIcon>
-            <StorageIcon style={{ color: 'white' }} />
+            <Database style={{ color: 'white' }} />
           </ListItemIcon>
           <ListItemText primary="Dataset" />
         </ListItem>
@@ -122,7 +112,7 @@ export default function MainListItems() {
 
           <ListItem button>
             <ListItemIcon>
-              <SettingsIcon style={{ color: 'white' }} />
+              <Settings style={{ color: 'white' }} />
             </ListItemIcon>
             <ListItemText primary="Settings" />
           </ListItem>
